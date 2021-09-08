3e8508fbc24ec8dc514c2e941a5367c6980c9d3d
Dylan Vernon <dylanvernon22@gmail.com>
2021-09-08 15:08:40 -0700

    Added header to the container div

--- container.js
+++ container.js
@@ -1,6 +1,8 @@
 class Container {
     constructor() {
         this.container = document.createElement('div');
+        this.title = document.createElement('h2');
+        this.title.innerHTML = 'Simple Simulation';
         this.container.className = 'container';
         this.container.style.position = 'relative';
         this.container.style.borderStyle = 'solid';
@@ -8,6 +10,7 @@ class Container {
         this.container.style.left = '11px';
         this.container.style.width = '500px';
         this.container.style.height = '500px';
-        body.appendChild(this.container);
+        this.container.appendChild(this.title);
+        document.body.appendChild(this.container);
     }
 }
