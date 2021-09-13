213476154dd9a58e8137b5bf333f2af4ac5b5bc6
Dylan Vernon <dylanvernon22@gmail.com>
2021-09-12 18:32:36 -0700

    Changed styles and changed the id of the navigation bars

--- navigationbar.js
+++ navigationbar.js
@@ -2,8 +2,8 @@ class NavigationBar {
     constructor(id, buttons) {
         this.buttons = [];
         this.element = document.createElement('nav');
-        this.element.id = id;
-        this.element.className = `${id}-navigation-bar`;
+        this.element.id = `${id}-navigation-bar`;
+        this.element.className = `navigation-bar`;
 
         for (let key in buttons) {
             let button = new Button(`${id}-button`, buttons[key], key);
