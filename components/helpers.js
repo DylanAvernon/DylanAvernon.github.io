9d2f8ab73b6823c91a4b7a221620e620aa720040
Dylan Vernon <dylanvernon22@gmail.com>
2021-10-24 19:54:01 -0700

    Added a featured project called String Permutor

--- components/helpers.js
+++ components/helpers.js
@@ -8,5 +8,21 @@ function getRandomColor() {
         blue: getRandomInt(255, 0)
     };
 }
-export { getRandomInt, getRandomColor };
+function permute(A) {
+    let len = A.length;
+    if (len === 1) {
+      return A;
+    }
+    let pick = "";
+    let permutations = [];
+    for (let i = 0; i < len; i++) {
+      pick = A[i];
+      var remainder = A.slice(0, i) + A.slice(i + 1, len);
+      for (var perm of permute(remainder)) {
+        permutations.push(pick + perm);
+      }
+    }
+    return permutations;
+}
+export { getRandomInt, getRandomColor, permute };
 
--- projects/projects.js
+++ projects/projects.js
@@ -5,6 +5,12 @@ import { randomWalkProperties } from "./simulations/random-walk-simulation.js";
 import { letterCounterProperties } from "./text-processors/letter-counter.js";
 import { wordCounterProperties } from "./text-processors/word-counter.js";
 import { sentenceCounterProperties } from "./text-processors/sentence-counter.js";
+import { stringPermutorProperties } from "./featured-projects/string-permutor.js";
+
+// Featured Projects
+let featuredProjects = document.getElementById('featured-projects');
+
+featuredProjects.appendChild(stringPermutorProperties.project.element);
 
 // Simulations
 let simulations = document.getElementById('simulations');
