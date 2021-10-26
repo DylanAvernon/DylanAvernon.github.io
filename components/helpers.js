43be328c0b891c84c1b166a2357a2094e3f61085
Dylan Vernon <dylanvernon22@gmail.com>
2021-10-26 07:24:45 -0700

    Added bus tracker to featured projects section and moved string perumtor to the text processors section

--- components/helpers.js
+++ components/helpers.js
@@ -1,6 +1,7 @@
 function getRandomInt(max, min) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
 function getRandomColor() {
     return {
         red: getRandomInt(255, 0), 
@@ -8,6 +9,7 @@ function getRandomColor() {
         blue: getRandomInt(255, 0)
     };
 }
+
 function permute(A) {
     let len = A.length;
     if (len === 1) {
@@ -24,5 +26,40 @@ function permute(A) {
     }
     return permutations;
 }
-export { getRandomInt, getRandomColor, permute };
+
+async function getBuses(route, apiKey){
+	const url = `https://api-v3.mbta.com/vehicles?filter[route]=${route}&include=trip&api_key=${apiKey}`;
+	const response = await fetch(url);
+	const json     = await response.json();
+	return json.data;
+}
+
+async function run(mbtaApiKey, colors, markers, map) {
+  let busObjects = [];
+  for (let route = 1; route <= 30; route++) {
+      let buses = await getBuses(route, mbtaApiKey);
+      if (buses.length > 0) {
+          let busObject = {
+              route: route,
+              buses: buses,
+              color: colors[route]
+          };
+          busObjects.push(busObject);
+      }
+  }
+  if (markers.length > 0) {
+      markers.forEach((marker) => marker.remove());
+  }
+
+  busObjects.forEach((busObject) => {
+      busObject.buses.forEach((bus) => {
+          let marker = new mapboxgl.Marker({color: busObject.color})
+              .setLngLat([bus.attributes.longitude, bus.attributes.latitude])
+              .addTo(map);
+          markers.push(marker);
+      });
+  });
+}
+
+export { getRandomInt, getRandomColor, permute, getBuses, run };
 
--- projects/projects.js
+++ projects/projects.js
@@ -6,18 +6,19 @@ import { letterCounterProperties } from "./text-processors/letter-counter.js";
 import { wordCounterProperties } from "./text-processors/word-counter.js";
 import { sentenceCounterProperties } from "./text-processors/sentence-counter.js";
 import { stringPermutorProperties } from "./featured-projects/string-permutor.js";
+import { busTrackerProperties } from "./featured-projects/bus-tracker.js";
 
 // Featured Projects
 let featuredProjects = document.getElementById('featured-projects');
 
-featuredProjects.appendChild(stringPermutorProperties.project.element);
+featuredProjects.appendChild(busTrackerProperties.project.element);
+featuredProjects.appendChild(pacmanSimulationProperties.project.element);
 
 // Simulations
 let simulations = document.getElementById('simulations');
 
 simulations.appendChild(projectileSimulationProperties.project.element);
 simulations.appendChild(randomWalkProperties.project.element);
-simulations.appendChild(pacmanSimulationProperties.project.element);
 
 // Text-Processors
 let textProcessors = document.getElementById('text-processors');
@@ -25,3 +26,4 @@ let textProcessors = document.getElementById('text-processors');
 textProcessors.appendChild(letterCounterProperties.project.element);
 textProcessors.appendChild(wordCounterProperties.project.element);
 textProcessors.appendChild(sentenceCounterProperties.project.element);
+textProcessors.appendChild(stringPermutorProperties.project.element);
